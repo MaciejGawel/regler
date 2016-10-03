17f84e369becfca02156e69a65e4ebfe7d32c675
Maciej Gawel <m.gawel@hotmail.com>
2016-10-03 22:47:36 +0200

    Config phoenix routes

--- web/router.ex
+++ web/router.ex
@@ -16,11 +16,6 @@ defmodule Regler.Router do
   scope "/", Regler do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
+    get "/*path", PageController, :index
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", Regler do
-  #   pipe_through :api
-  # end
 end
