fffe9ddb2aa8ac366a29b2170da84d29d87d7371
Maciej Gawel <m.gawel@hotmail.com>
2016-10-05 21:24:55 +0200

    Create users. Install Comeonin - password hashing library

--- mix.exs
+++ mix.exs
@@ -17,9 +17,20 @@ defmodule Regler.Mixfile do
   #
   # Type `mix help compile.app` for more information.
   def application do
-    [mod: {Regler, []},
-     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+    [
+      mod: {Regler, []},
+      applications: [
+        :phoenix,
+        :phoenix_pubsub,
+        :phoenix_html,
+        :cowboy,
+        :logger,
+        :gettext,
+        :phoenix_ecto,
+        :postgrex,
+        :comeonin
+      ]
+    ]
   end
 
   # Specifies which paths to compile per environment.
@@ -30,14 +41,17 @@ defmodule Regler.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.1"},
-     {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_ecto, "~> 3.0"},
-     {:postgrex, ">= 0.0.0"},
-     {:phoenix_html, "~> 2.6"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+    [
+      {:phoenix, "~> 1.2.1"},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:phoenix_ecto, "~> 3.0"},
+      {:postgrex, ">= 0.0.0"},
+      {:phoenix_html, "~> 2.6"},
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:gettext, "~> 0.11"},
+      {:cowboy, "~> 1.0"},
+      {:comeonin, "~> 2.5"}
+    ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
